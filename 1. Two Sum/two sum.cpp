51ebe46f5a2085623e6d5368c87e2c42558b0238
IsaacAsante <isaac.editor16@gmail.com>
2023-01-24 04:42:24 +0800

    First solution uploaded. Two Sum problem.

---
+++ 1. Two Sum/two sum.cpp
@@ -0,0 +1,34 @@
+/* Author: Isaac Asante
+ * LeetCode URL for this problem: https://leetcode.com/problems/two-sum/
+ * Original video explanation: Pending
+ * Last verified on: January 24, 2023
+ */
+
+ /* IMPORTANT:
+  * C++ headers are missing from the code below. 
+  * Only the Solution class is included.
+  */
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+        int first_index, second_index; // Use to store correct indices
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            /* Is there another saved value (in the map), which amounts
+             * to the target when added to the current one?
+             */
+            unordered_map<int, int>::iterator it = m.find(target - nums[i]);
+            if (it != m.end()) {
+                first_index = it->second; // Save the other index
+                second_index = i; // Save current index
+                break; // Exit immediately
+            }
+            // Otherwise, update normally
+            m[nums[i]] = i;
+        }
+        return vector<int>{first_index, second_index};
+    }
+};
